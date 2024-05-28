67efc032750bbdd71ed08dbaaa9274acc6a419eb
didisxm97166 <dylanbontemps@hotmail.fr>
2024-05-28 20:58:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+@import '../styles/css/main.css';
+</style>
