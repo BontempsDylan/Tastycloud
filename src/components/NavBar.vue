8704ff6f40f5766b0f80c4b3e1d58caaf5242353
didisxm97166 <dylanbontemps@hotmail.fr>
2024-05-29 16:10:13 +0200

    Création du navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav id="navbar">
+        <div>
+            <a href="/">Acceuil</a>
+            <a href="/">plats<font-awesome-icon :icon="['fas', 'chevron-down']" /></a>
+            <a href="/">Dessert<font-awesome-icon :icon="['fas', 'chevron-down']" /></a>
+        </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'NavbarComponent',
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <HeaderComponent />
+    <NavbarComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from '@/components/Header.vue'
+import NavbarComponent from '@/components/NavBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HeaderComponent,
+    NavbarComponent,
   },
 };
 </script>
