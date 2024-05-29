997b39edadfcf341d9208476603ef800e7176000
didisxm97166 <dylanbontemps@hotmail.fr>
2024-05-29 22:37:57 +0200

    Création du footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="footer">
+      <div class="container">
+        <p>&copy; 2024 TastyCloud. Tous droits réservés.</p>
+        <nav>
+          <ul>
+            <li><router-link to="/">Acceuil</router-link></li>
+            <li><router-link to="/Plats">plats</router-link></li>
+            <li><router-link to="/desserts">Dessert</router-link></li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+};
+</script>
